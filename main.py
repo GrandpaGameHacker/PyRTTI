46ef1765e3936183013508d2ac11edf30176f2f7
Samson <samsonpianofingers@users.noreply.github.com>
2020-03-29 19:23:33 +1100

    Fixed columns, added column sort added export data added context menu  - copy selected data  - dummy menu item for find code references

--- main.py
+++ main.py
@@ -4,6 +4,57 @@ from tkinter import filedialog
 import ntpath
 import rtti
 
+class ClassViewer(ttk.Treeview):
+    def __init__(self, parent, *args, **kwargs):
+        ttk.Treeview.__init__(self, parent, *args, **kwargs)
+
+        self.context_menu = tk.Menu(self, tearoff=0)
+        self.context_menu.add_command(label="Copy Selected", command=self.copy_selected)
+        self.context_menu.add_command(label="Find Code References...",
+                                      command=self.find_references)
+        self.bind("<Button-3>", self.popup)
+
+
+    def popup(self, event):
+        try:
+            self.context_menu.tk_popup(event.x_root+73, event.y_root+10, 0)
+        finally:
+            self.context_menu.grab_release()
+
+
+    def to_str(self):
+        text = ""
+        item = self.get_children('')
+        for i in item:
+            text += ' | '.join(self.item(i, 'values'))
+            text += "\n"
+        return text
+
+
+    def copy_selected(self):
+        self.clipboard_clear()
+        text = ""
+        item = self.selection()
+        for i in item:
+            text += ' | '.join(self.item(i, 'values'))
+            text += "\n"
+        self.clipboard_append(text)
+
+    def find_references(self):
+        pass
+
+    def sort_column(self, tv, col, reverse):
+        l = [(tv.set(k, col), k) for k in tv.get_children('')]
+        l.sort(reverse=reverse)
+
+        # rearrange items in sorted positions
+        for index, (val, k) in enumerate(l):
+            tv.move(k, '', index)
+
+        # reverse sort next time
+        tv.heading(col, command=lambda: \
+                   self.sort_column(tv, col, not reverse))
+
 
 class PyClassInformer(tk.Frame):
     def __init__(self, root, *args, **kwargs):
@@ -23,28 +74,32 @@ class PyClassInformer(tk.Frame):
         self.root.config(menu=self.menubar)
 
         # controls
-        self.lbl_file = tk.Label(text="TARGET*")
-
+        self.lbl_file = tk.Label(text="No File Selected.")
         self.scrollbar = tk.Scrollbar(self.root)
         self.scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
 
-        # classlist
-        self.classlist = ttk.Treeview(self.root,
+        # classlist control
+        self.classlist = ClassViewer(self.root,
                                       yscrollcommand=self.scrollbar.set)
-        self.classlist['columns'] = ('#1')
-        self.classlist.heading("#0", text="vftable_addr", anchor=tk.W)
-        self.classlist.heading("#1", text="symbol", anchor=tk.W)
-        self.classlist.column("#0", width=150, minwidth=150, stretch=tk.YES)
-        self.classlist.column("#1", width=270, minwidth=270, stretch=tk.YES)
+        self.classlist['columns'] = ('#1','#2')
+        for col in self.classlist['columns']:
+            self.classlist.heading(col, text=col, command=lambda _col=col: \
+                     self.classlist.sort_column(self.classlist, _col, True))
+
+        self.classlist.heading("#0", text="ID", anchor=tk.W)
+        self.classlist.heading("#1", text="VFTable VA", anchor=tk.W)
+        self.classlist.heading("#2", text="Class", anchor=tk.W)
+        self.classlist.column("#0", width=35, minwidth=35, stretch=tk.YES)
+        self.classlist.column("#1", width=150, minwidth=150, stretch=tk.YES)
+        self.classlist.column("#2", width=270, minwidth=270, stretch=tk.YES)
 
         # gui style
         self.lbl_file.pack(anchor="w")
         self.classlist.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
         self.scrollbar.config(command=self.classlist.yview)
-        # app variables
-        self.file_path = None
 
     def loadFile(self):
+        self.classlist.delete(*self.classlist.get_children())
         self.file_path = filedialog.askopenfilename(
             filetypes=(("Executable files", "*.exe"),
                        ("DLL Library", "*.dll"), ("all files", "*.*")))
@@ -57,11 +112,23 @@ class PyClassInformer(tk.Frame):
         self.scanner.scan()
         for i in range(len(self.scanner.vftables)):
             self.classlist.insert("", i,
-                                  text=str(hex(self.scanner.vftables[i])),
-                                  values=(self.scanner.symbols[i].replace(" ", "\ ")))
+                                  text=str(i),
+                                  values=(hex(self.scanner.vftables[i]),self.scanner.symbols[i].replace(" ", "\ ")))
 
     def exportData(self):
-        pass
+        save_file_path = filedialog.asksaveasfilename()
+        if save_file_path == None:
+            print("Error - Invalid export path")
+            return
+        try:
+            with open(save_file_path, 'w') as file:
+                data = self.classlist.to_str()
+                file.write(data)
+                file.close()
+        finally:
+            return
+
+
 
 
 def main():
